fe9e1d7243c446e3990d9250a4ec039e3f09f351
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-03 13:56:17 -1000

    setup complete

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ const products = require('./products');
 
 const router = express.Router();
 
-router.use('/articles', articles);
+router.use('/users', users);
 router.use('/products', products);
+router.use('/cart', cart);
 
 module.exports = router;
