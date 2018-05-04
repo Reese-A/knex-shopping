16185958eb6fe3925c2c29d09c686b85c23c90ac
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-03 16:11:35 -1000

    users get working

--- routes/users/index.js
+++ routes/users/index.js
@@ -4,20 +4,43 @@ const knex = require('../../db/knex.js');
 
 router.route('/')
 
+router.route('/:user_id')
+  .get((req, res) => {
+    const user = req.params.user_id;
+    console.log(user);
+    return knex
+      .raw(
+        'SELECT * FROM users WHERE id = ?',
+        [user]
+      )
+      .then((data) => {
+        if(!data.rows[0]){
+          res.status(404);
+          throw err;
+        }
+        return res.json(data.rows[0]);
+      })
+      .catch((err) => {
+        return res.json({
+          'message': 'User not found'
+        })
+      });
+  });
+
 router.route('/register')
   .post((req, res) => {
     // return new Promise (function(resolve, reject){
-      const email = req.body.email.toLowerCase();
-      const password = req.body.password;
-      return knex
+    const email = req.body.email.toLowerCase();
+    const password = req.body.password;
+    return knex
       .raw(
         'INSERT INTO users (email, password) VALUES (?,?) RETURNING *', [email, password]
       )
       .then((data) => {
         const user = data.rows[0];
         // if (!user.email || !user.password) {
-          //   return res.json({
-            //     'message': 'Please fill out all fields before submitting'
+        //   return res.json({
+        //     'message': 'Please fill out all fields before submitting'
         //   })
         // }
         return res.json({
