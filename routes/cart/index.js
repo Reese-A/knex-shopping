6c0594b0b2573ac8e804840cc6a9d5d01f8c4548
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-04 11:34:38 -1000

    cart delete working

--- routes/cart/index.js
+++ routes/cart/index.js
@@ -3,16 +3,15 @@ const router = express.Router();
 const knex = require('../../db/knex.js');
 
 router.route('/:user_id')
-  .get((req,res) => {
+  .get((req, res) => {
     const userId = req.params.user_id;
     return knex
       .raw(
-        'SELECT products.* FROM cart INNER JOIN users ON cart.user_id = users.id INNER JOIN products ON cart.product_id = products.id WHERE users.id = ?',
-        [userId]
+        'SELECT products.* FROM cart INNER JOIN users ON cart.user_id = users.id INNER JOIN products ON cart.product_id = products.id WHERE users.id = ?', [userId]
       )
-      .then((data) =>{
+      .then((data) => {
         const cart = data.rows;
-        if(cart.length == 0){
+        if (cart.length == 0) {
           return res.json({
             'message': 'No existing cart found for this user or user does not exist'
           })
@@ -22,9 +21,9 @@ router.route('/:user_id')
       .catch((err) => {
         return res.json({
           'message': 'ERROR'
-        })
-      })
-  })
+        });
+      });
+  });
 
 router.route('/:user_id/:product_id')
   .post((req, res) => {
@@ -46,6 +45,31 @@ router.route('/:user_id/:product_id')
           'message': 'Invalid user or product'
         });
       })
+  })
+
+  .delete((req, res) => {
+    const userId = req.params.user_id;
+    const productId = req.params.product_id;
+    return knex
+      .raw(
+        'DELETE FROM cart WHERE user_id = ? AND product_id = ? RETURNING *', [userId, productId]
+      )
+      .then((data) => {
+        const cart = data.rows;
+        if (cart.length === 0) {
+          return res.json({
+            'message': 'cart could not be found'
+          })
+          return res.json({
+            "success": true
+          })
+        }
+      })
+      .catch((err) => {
+        return res.json({
+          'message': 'ERROR'
+        });
+      });
   })
 
 module.exports = router;
