bdc8b339b71c93412b51db1c8b17f89d0470e674
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-03 18:08:54 -1000

    user password put working

--- routes/users/index.js
+++ routes/users/index.js
@@ -7,25 +7,51 @@ router.route('/')
 router.route('/:user_id')
   .get((req, res) => {
     const user = req.params.user_id;
-    console.log(user);
     return knex
       .raw(
-        'SELECT * FROM users WHERE id = ?',
-        [user]
+        'SELECT * FROM users WHERE id = ?', [user]
       )
       .then((data) => {
-        if(!data.rows[0]){
-          res.status(404);
-          throw err;
+        if (!data.rows[0]) {
+          return res.status(404).json({
+            'message': 'User not found'
+          })
         }
         return res.json(data.rows[0]);
       })
       .catch((err) => {
-        return res.json({
-          'message': 'User not found'
-        })
+        res.send('oh no');
       });
   });
+
+
+router.route('/login')
+  .post((req, res) => {
+    const email = req.body.email.toLowerCase();
+    const password = req.body.password;
+    return knex
+      .raw(
+        'SELECT * FROM users WHERE email = ?', [email]
+      )
+      .then((data) => {
+        const user = data.rows[0];
+        if (!user) {
+          return res.status(404).json({
+            'message': 'user does not exist'
+          })
+        }
+        if (password !== user.password) {
+          return res.json({
+            'message': 'incorrect password'
+          })
+        }
+        return res.json(user);
+      })
+      .catch((err) => {
+        res.send('you fucked up')
+      });
+  });
+
 
 router.route('/register')
   .post((req, res) => {
@@ -54,6 +80,31 @@ router.route('/register')
         return res.json({
           "message": "User already exists"
         });
+      });
+  });
+
+
+router.route('/:user_id/forgot-password')
+  .put((req, res) => {
+    const userId = req.params.user_id;
+    const password = req.body.password;
+    return knex
+      .raw(
+        'UPDATE users SET password = ? WHERE id = ? RETURNING *', [password, userId]
+      )
+      .then((data) => {
+        const user = data.rows[0];
+        if (!user) {
+          return res.json({
+            "message": "New password created!"
+          })
+        }
+        return res.status(404).json({
+          "message": "User ID not found"
+        })
+      })
+      .catch((err)=>{
+        return res.send('terrible things have happened')
       });
   });
 
